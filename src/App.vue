8dc17586d7337d449a34f93ca1d152298670838a
meiry <naively3@gmail.com>
2024-12-02 23:48:03 +0900

    tarot

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import CardTemplate from './components/CardTemplete.vue'
+
+</script>
+
+<template>
+  <div id="app">
+    <CardTemplate />
+  </div>
+
+</template>
+
+<style scoped>
+  body{
+    margin: 0;
+  }
+
+
+</style>
---
+++ src/components/CardTemplete.vue
@@ -0,0 +1,26 @@
+
+
+<template>
+  <div id="CardNum" class="card"></div>
+</template>
+
+<style scoped>
+  #CardNum{
+    height: 200px;
+    width: 150px;
+    background-color: black;
+  }
+</style>
+
+<script>
+
+export default {
+  name: 'CardTemplate',     // 1. 컴포넌트의 이름을 지정해요
+  props: {},
+  data() {
+    return {
+      CardName: "1번 카드"
+    }
+  },
+}
+</script>
