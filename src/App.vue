4dd2dcc8e3539f3eeeec508d20f031e5275aa103
meiry <naively3@gmail.com>
2024-12-06 15:44:00 +0900

    배경 이미지(이미수정필)

--- src/App.vue
+++ src/App.vue
@@ -13,15 +13,6 @@
 </template>
 
 <style scoped>
-  .background {
-    margin: 0;
-    background: url('src/assets/landing_logo.jpg') repeat ;
-    top:0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    position: fixed;
-    padding:0;
-  }
+
 
 </style>
